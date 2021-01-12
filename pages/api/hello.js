586e4f2bb23d708c36c9acba5fbc7d500373024a
Nikita Schastnyy <smynay@yandex.ru>
2021-01-12 17:28:57 +0300

    Initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+export default function hello(req, res) {
+  console.log('Hello from SSR');
+  res.statusCode = 200;
+  res.setHeader = ('Content-Type', 'application/json');
+  res.end(
+    JSON.stringify({
+      message: 'Hello from SSR',
+    })
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Grid from '@material-ui/core/Grid';
+import { red } from '@material-ui/core/colors';
 
-export default function Home() {
+const customStyle = {
+  textRed: {
+    color: red[500],
+  },
+};
+
+export default function Page({ apiResponse }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <HelloAuxillary color={customStyle.textRed}>
+      <p>I'm text from a component</p>
+      <p>{apiResponse}</p>
+    </HelloAuxillary>
+  );
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+Page.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return null;
+  }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  const response = await fetch(`http://localhost:3000/api/hello`);
+  const result = await response.json();
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  return {
+    apiResponse: result.message,
+  };
+};
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+function HelloAuxillary({ children, color }) {
+  return (
+    <>
+      <h1 style={color}>Hello World!</h1>
+      <Grid>
+        <Grid item xs={12}>
+          {children}
+        </Grid>
+      </Grid>
+    </>
+  );
 }
